51e0f7a7ece1b45a55f1eed17abdb31532c5b2c2
DanielR59 <daniel_pumas_59@hotmail.com>
2022-03-13 23:38:17 -0600

    support 3.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.rst").read_text()
 # This call to setup() does all the work
 setup(
     name="target_describe",
-    version="0.0.1",
+    version="0.0.3",
     description="Generate viz for your variables with your target for ML",
     long_description=README,
     url="https://github.com/DanielR59/target_description",
@@ -17,6 +17,7 @@ setup(
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(exclude=("tests",)),
